f3ba438a7e9c30b591ac913dd9c30232d19dc2df
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-06-05 19:52:30 +0200

    [REWORK] from scratch, POO, 4 tests ok at this step

--- src/playground/playground.ts
+++ src/playground/playground.ts
@@ -37,87 +37,94 @@ import {
  * Fonction qui est lancé à chaque lancement de partie.
  */
 export default function* playground() {
+
   // Lecture des données de base du jeu :
-  const baseData = yield* readLine(); // "<base latitude> <base longitude> <base attack range> <base energy>"
-  const baseLatitude: number = Number(baseData.split(" ")[0])
-  const baseLongitude: number = Number(baseData.split(" ")[1])
-  const baseAttackRange: number = Number(baseData.split(" ")[2])
-  const baseEnergy: number = Number(baseData.split(" ")[3])
+  const baseData: string = yield* readLine(); // "<base latitude> <base longitude> <base attack range> <base energy>"
 
-  const baseAttackRangeConverted = baseAttackRange / 100000
-  console.log(baseAttackRangeConverted)
-
-  console.log("baseData\n", "latitude: " + baseLatitude + "\n", "Longitude: " + baseLongitude + "\n", "Attack Range: " + baseAttackRange + "\n", "Energy: " + baseEnergy + "\n");
+  // INIT - Création de l'objet BASE :
+  const base = {
+    y: +baseData.split(" ")[0],       // latitude (y)
+    x: +baseData.split(" ")[1],       // longitude (x)
+    range: +baseData.split(" ")[2], 
+    energy: +baseData.split(" ")[3],
+  }
 
   const nbActors = yield* readLine(); // "<nb actors>"
-  console.log("nbActors", nbActors);
+  // INIT - Création d'une liste vide pour y stocker nos objets Actors :
+  let actorsList = []
 
-  let rabbitsIdList = []
 
   // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ?
   for (let i = 0; i < Number(nbActors); ++i) {
     const actor = yield* readLine(); // "<actor id> <actor type (robot|rabbit)> <actor speed (km/h)>"
-    const actorId = actor.split(" ")[0]
-    const actorType = actor.split(" ")[1]
-    const actorSpeed = actor.split(" ")[2]
-    console.log("actorSplit init", actorId, actorType, actorSpeed);
 
-    if (actorType === "rabbit") {
-      rabbitsIdList.push(actorId)
-    }
+    // Création de l'objet ACTOR de cette itération et on le place dans notre liste (actorList) :
+    actorsList.push(
+      {
+        id: actor.split(" ")[0],
+        type: actor.split(" ")[1],
+        speed: +actor.split(" ")[2],
+        x: null,
+        y: null,
+        alive: true
+      }
+    )
   }
+
+  // Création d'un compteur de tour :
+  let turn = 0
+
+  // *************** Fin de L'INITIALISATION *****************
 
   // Code exécuté à chaque tour
   while (true) {
+
+    // On incrémente le tour de 1
+    turn++
+
+    // On initialise la cible à null ou l'on supprime la cible précédente si il y a :
+    let nextTarget = null
+
     // A chaque tour, on récupère les mises à jour de chaque entités (statut
     // vivant ou mort, nouvelle position, ...)
     // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ?
-    let currentTarget = ""
-    let isInRange = false
-    let savedLatitudeDiff = 10000
-    let savedLongitudeDiff = 10000
-
     for (let i = 0; i < Number(nbActors); ++i) {
       const actor = yield* readLine(); // "<actor id> <actor status (alive|dead)> <actor latitude> <actor longitude>"
+
+      // on récupère l'id de l'actor pour savoir qui on met à jour :
       const actorId = actor.split(" ")[0]
-      const actorStatus = actor.split(" ")[1]
-      const actorLatitude: number = Number(actor.split(" ")[2])
-      const actorLongitude: number = Number(actor.split(" ")[3])
 
-      const latitudeDiff = actorLatitude - baseLatitude
-      const longitudeDiff = actorLongitude - baseLongitude
-      console.log(latitudeDiff, longitudeDiff)
-
-      console.log("actorSplit update", actorId, actorStatus, actorLatitude, actorLongitude);
-      if (
-        actorLatitude > baseLatitude - baseAttackRangeConverted &&
-        actorLatitude < baseLatitude + baseAttackRangeConverted &&
-        actorLongitude > baseLongitude - baseAttackRangeConverted &&
-        actorLongitude < baseLongitude + baseAttackRangeConverted
-      ) {
-        isInRange = true
-      }
-
-
-      if (actorStatus === "alive" && !rabbitsIdList.includes(actorId) && isInRange) {
-        if (latitudeDiff<savedLatitudeDiff && longitudeDiff<savedLongitudeDiff) {
-          savedLatitudeDiff = latitudeDiff
-          savedLongitudeDiff = longitudeDiff
-          currentTarget = actorId
-          console.log("good target::::" + actorId)
+      // on boucle sur notre liste (actorList) pour trouver l'actor et on le met à jour :
+      actorsList.forEach((a, index) => {
+        if (a.id === actorId) {
+          actorsList[index] = {
+            ...a, 
+            alive: actor.split(" ")[1] === "alive" ? true : false,
+            y: +actor.split(" ")[2],
+            x: +actor.split(" ")[3]
+          }
         }
-      }
+      })
     }
+
     // Après avoir reçu les mises à jour, on doit effectuer une (ET UNE SEULE) action:
     // - `yield* wait()` : On ne fait rien (on passe notre tour)
     // - `yield* shotTarget('nemo');` : On décide de tirer sur l'entité qui a l'id "nemo"
     // yield* wait();
-    if (currentTarget === "") {
-      yield* wait()
-    } else {
-      yield* shotTarget(currentTarget)
-      console.log("IS DEADDD NOW  -  " + currentTarget)
-      currentTarget=""
-    }
+    console.log("::::::::::::::")
+    console.log(turn)
+    console.log("::::::::::::::")
+    console.log(actorsList)
+
+
+    // Création de la logique pour définir la cible idéale lors de ce tour :
+    actorsList.forEach(a=>{
+      if (a.type === "robot" && a.alive === true)
+      return nextTarget = a.id
+    })
+
+    // Si une cible est sélectionnée, on l'éxecute, Sinon, on passe au prochain tour :
+    nextTarget ? yield* shotTarget(nextTarget): yield* wait()
+
   }
 }
