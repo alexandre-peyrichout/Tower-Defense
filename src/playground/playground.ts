31b4f32b5e84432a0de21646fcf0ce5edbd78c26
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-06-06 05:18:21 +0200

    [REFACTO] - dispatch feature moved into a function

--- src/playground/playground.ts
+++ src/playground/playground.ts
@@ -52,7 +52,7 @@ export default function* playground() {
 
   const nbActors = yield* readLine(); // "<nb actors>"
   // INIT - Création d'une liste vide pour y stocker nos objets Actors :
-  const actorsList = []
+  const actorsList: Array<any> = []
 
 
   // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ? Conversion de type pour itérer avec "+".
@@ -70,7 +70,7 @@ export default function* playground() {
   // INIT - Création d'un compteur de tour :
   let turn = 0
 
-  // INIT - Création de la fonction qui permet de définir si le robot est dans la zone de tir :
+  // INIT - Création de la fonction qui permet de définir si l'actor est dans la zone de tir :
   const isInRange = (actor_x: number, actor_y: number) => {
     if ((actor_x - base.x) * (actor_x - base.x) +
       (actor_y - base.y) * (actor_y - base.y) <= base.range * base.range) {
@@ -107,6 +107,31 @@ export default function* playground() {
     )
     return possibleTargets[minIndex]
   }
+  // Création de la fonction de dispatch pour gérer l'identification, la mise à jour et le transfert d'un actor dans la liste des cibles potentielles :
+  const dispatch = (actor: string, possibleTargets: Array<any>) => {
+    // RUN - on récupère l'id de l'actor pour savoir qui on met à jour :
+    const currentId = actor.split(" ")[0]
+    // RUN - on stock les autres infos de manière lisible :
+    const isStillAlive = actor.split(" ")[1] === "alive" ? true : false
+    const currentY = +actor.split(" ")[2]
+    const currentX = +actor.split(" ")[3]
+    const canBeShot = isInRange(currentX, currentY)
+
+    // RUN - on cherche l'acteur dans notre liste initiale :
+    const currentActor = actorsList.find(actor => actor.id === currentId)
+    // On vérifie si il remplit les conditions pour être une cible :
+    if (currentActor && currentActor.type === "robot" && isStillAlive && canBeShot) {
+      // on envoie l'objet mis à jour au complet dans la liste des cibles potentielles
+      possibleTargets.push({
+        ...currentActor,
+        y: currentY,
+        x: currentX,
+        distance: distanceFromBase(currentX, currentY)
+      })
+    }
+  }
+
+
   // DEBUG - infos :
   console.debug("DEBUT DE PARTIE")
   console.debug(`initialisation de ${actorsList.length} acteurs`)
@@ -118,15 +143,15 @@ export default function* playground() {
   // RUN - Code exécuté à chaque tour
   while (true) {
 
-    // RUN - On incrémente le tour de 1 :
-    turn++
-    console.debug("Energie disponible", base.energy)
-
-    // RUN - Création d'une liste temporaire vide pour y stocker nos cibles potentielles (objets) :
-    const possibleTargets: Array<any> = []
-
     // RUN - Initialisation de la cible sélectionnée à vide en début de tour :
     let selectedTarget = null
+    
+    // RUN - Création d'une liste temporaire vide pour y stocker nos cibles potentielles (objets) :
+    const possibleTargets: Array<any> = []
+    
+    // DEBUG - infos:
+    turn++
+    console.debug(`tour [ ${turn} ] : Energie disponible`, base.energy)
 
     // A chaque tour, on récupère les mises à jour de chaque entités (statut
     // vivant ou mort, nouvelle position, ...)
@@ -134,41 +159,25 @@ export default function* playground() {
     for (let i = 0; i < +nbActors; ++i) {
       const actor = yield* readLine(); // "<actor id> <actor status (alive|dead)> <actor latitude> <actor longitude>"
 
-      // RUN - on récupère l'id de l'actor pour savoir qui on met à jour :
-      const currentId = actor.split(" ")[0]
-      // RUN - on stock les autres infos de manière lisible :
-      const isStillAlive = actor.split(" ")[1] === "alive" ? true : false
-      const currentY = +actor.split(" ")[2]
-      const currentX = +actor.split(" ")[3]
-      const canBeShot = isInRange(currentX, currentY)
-
-      // RUN - on cherche l'acteur dans notre liste initiale :
-      const currentActor = actorsList.find(actor => actor.id === currentId)
-      // On vérifie si il remplit les conditions pour être une cible :
-      if (currentActor && currentActor.type === "robot" && isStillAlive && canBeShot) {
-        // on envoie l'objet mis à jour au complet dans la liste des cibles potentielles
-        possibleTargets.push({
-          ...currentActor,
-          y: currentY,
-          x: currentX,
-          distance: distanceFromBase(currentX, currentY)
-        })
-      }
+      // RUN - Pour chaque ligne "actor", on envoie le résultat de la lecture dans le dispatch :
+      dispatch(actor, possibleTargets)
     }
 
     // Après avoir reçu les mises à jour, on doit effectuer une (ET UNE SEULE) action:
     // - `yield* wait()` : On ne fait rien (on passe notre tour)
     // - `yield* shotTarget('nemo');` : On décide de tirer sur l'entité qui a l'id "nemo"
 
-    // On cherche la cible la plus proche de la base parmi les cibles potentielles et on la sélectionne :
+    // RUN - On cherche la cible la plus proche de la base parmi les cibles potentielles et on la sélectionne :
     selectedTarget = getClosestTarget(possibleTargets)
 
     // DEBUG - infos:
-    console.debug(`cibles potentielles du tour n° ${turn}`, possibleTargets)
+    possibleTargets.length ?
+      console.debug(`tour [ ${turn} ] : `, possibleTargets.length, `cible${possibleTargets.length > 1 ? "s" : ""} potentielle${possibleTargets.length > 1 ? "s" : ""}`, possibleTargets) :
+      console.debug(`tour [ ${turn} ] : Standby`)
 
     // Si une cible est sélectionnée, on l'éxecute, Sinon, on passe au prochain tour :
     if (selectedTarget && turn > 1) {
-      console.debug(`victime au tour n° ${turn} >>>>>`, selectedTarget.id, "<<<<<")
+      console.debug(`tour [ ${turn} ] : ${selectedTarget.id.toUpperCase()} has been killed`)
       yield* shotTarget(selectedTarget.id)
       base.energy--
     } else {
