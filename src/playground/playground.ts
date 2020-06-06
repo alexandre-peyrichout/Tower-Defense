6ec01d2b33f96ca2062ec3e3d593b32373c86f3d
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-06-06 03:57:40 +0200

    [REFACTO] - cleaning code and remove useless loops

--- src/playground/playground.ts
+++ src/playground/playground.ts
@@ -46,37 +46,32 @@ export default function* playground() {
   const base = {
     y: +baseData.split(" ")[0],       // latitude (y)
     x: +baseData.split(" ")[1],       // longitude (x)
-    range: +baseData.split(" ")[2] / 100000, // Converted to match coordinates
+    range: +baseData.split(" ")[2] / 100000, // Conversion pour s'ajuster aux valeurs de longitude / latitude
     energy: +baseData.split(" ")[3],
   }
+
   const nbActors = yield* readLine(); // "<nb actors>"
   // INIT - Création d'une liste vide pour y stocker nos objets Actors :
-  let actorsList = []
+  const actorsList = []
 
 
-  // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ?
-  for (let i = 0; i < Number(nbActors); ++i) {
+  // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ? Conversion de type pour itérer avec "+".
+  for (let i = 0; i < +nbActors; ++i) {
     const actor = yield* readLine(); // "<actor id> <actor type (robot|rabbit)> <actor speed (km/h)>"
 
-    // Création de l'objet ACTOR de cette itération et on le place dans notre liste (actorList) :
-    actorsList.push(
-      {
-        id: actor.split(" ")[0],
-        type: actor.split(" ")[1],
-        speed: +actor.split(" ")[2],
-        x: null,
-        y: null,
-        alive: true,
-        isInRange: false
-      }
-    )
+    // INIT - Création de l'objet ACTOR de cette itération et on le place dans notre liste (actorList) :
+    actorsList.push({
+      id: actor.split(" ")[0],
+      type: actor.split(" ")[1],
+      speed: +actor.split(" ")[2]
+    })
   }
 
-  // Création d'un compteur de tour :
+  // INIT - Création d'un compteur de tour :
   let turn = 0
 
-  // Création de la fonction qui permet de définir si le robot est dans la zone de tir:
-  const isInside = (actor_x: number, actor_y: number) => {
+  // INIT - Création de la fonction qui permet de définir si le robot est dans la zone de tir :
+  const isInRange = (actor_x: number, actor_y: number) => {
     if ((actor_x - base.x) * (actor_x - base.x) +
       (actor_y - base.y) * (actor_y - base.y) <= base.range * base.range) {
       return true
@@ -86,61 +81,98 @@ export default function* playground() {
     }
   }
 
+  // INIT - Création de la fonction qui calcule la distance entre l'actor et la base :
+  const distanceFromBase = (actor_x: number, actor_y: number) => {
+    const a = actor_x - actor_y;
+    const b = base.x - base.y;
+    const distance = Math.sqrt(a * a + b * b);
+    return distance
+  }
 
+  // INIT - Création de la fonction qui, parmi une liste, retourne l'objet dont la propriété "distance" est la plus faible:
+  const getClosestTarget = (possibleTargets: Array<any>) => {
 
+    let minIndex: number = 0;
+    let minValue: number;
+
+    possibleTargets.forEach((target, index) => {
+      if (index === 0) {
+        minIndex = index
+        minValue = target.distance
+      } else if (target.distance < minValue) {
+        minValue = target.distance
+        minIndex = index
+      }
+    }
+    )
+    return possibleTargets[minIndex]
+  }
+  // DEBUG - infos :
+  console.debug("DEBUT DE PARTIE")
+  console.debug(`initialisation de ${actorsList.length} acteurs`)
+  actorsList.forEach((actor, index) => {
+    console.debug(`n° ${index + 1}: ${actor.id} | ${actor.type === "robot" ? "robot" : "lapin"} | ${actor.speed} km/h`)
+  })
   // *************** Fin de L'INITIALISATION *****************
 
-  // Code exécuté à chaque tour
+  // RUN - Code exécuté à chaque tour
   while (true) {
 
-    // On incrémente le tour de 1
+    // RUN - On incrémente le tour de 1 :
     turn++
+    console.debug("Energie disponible", base.energy)
 
-    // On initialise la cible à null ou l'on supprime la cible précédente si il y a :
+    // RUN - Création d'une liste temporaire vide pour y stocker nos cibles potentielles (objets) :
+    const possibleTargets: Array<any> = []
+
+    // RUN - Initialisation de la cible sélectionnée à vide en début de tour :
     let selectedTarget = null
 
     // A chaque tour, on récupère les mises à jour de chaque entités (statut
     // vivant ou mort, nouvelle position, ...)
-    // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ?
-    for (let i = 0; i < Number(nbActors); ++i) {
+    // @ts-ignore TODO possible de corriger l'erreur sur la ligne suivante ? Conversion de type pour itérer avec "+".
+    for (let i = 0; i < +nbActors; ++i) {
       const actor = yield* readLine(); // "<actor id> <actor status (alive|dead)> <actor latitude> <actor longitude>"
 
-      // on récupère l'id de l'actor pour savoir qui on met à jour :
-      const actorId = actor.split(" ")[0]
+      // RUN - on récupère l'id de l'actor pour savoir qui on met à jour :
+      const currentId = actor.split(" ")[0]
+      // RUN - on stock les autres infos de manière lisible :
+      const isStillAlive = actor.split(" ")[1] === "alive" ? true : false
+      const currentY = +actor.split(" ")[2]
+      const currentX = +actor.split(" ")[3]
+      const canBeShot = isInRange(currentX, currentY)
 
-      // TEST 5: On ajoute une nouvelle propriété qui définit si oui ou non la l'actor est à portée de tir :
-
-
-      // on boucle sur notre liste (actorList) pour trouver l'actor et on le met à jour :
-      actorsList.forEach((a, index) => {
-        if (a.id === actorId) {
-          actorsList[index] = {
-            ...a,
-            alive: actor.split(" ")[1] === "alive" ? true : false,
-            y: +actor.split(" ")[2],
-            x: +actor.split(" ")[3],
-            isInRange: isInside(+actor.split(" ")[3], +actor.split(" ")[2])
-          }
-        }
-      })
+      // RUN - on cherche l'acteur dans notre liste initiale :
+      const currentActor = actorsList.find(actor => actor.id === currentId)
+      // On vérifie si il remplit les conditions pour être une cible :
+      if (currentActor && currentActor.type === "robot" && isStillAlive && canBeShot) {
+        // on envoie l'objet mis à jour au complet dans la liste des cibles potentielles
+        possibleTargets.push({
+          ...currentActor,
+          y: currentY,
+          x: currentX,
+          distance: distanceFromBase(currentX, currentY)
+        })
+      }
     }
 
     // Après avoir reçu les mises à jour, on doit effectuer une (ET UNE SEULE) action:
     // - `yield* wait()` : On ne fait rien (on passe notre tour)
     // - `yield* shotTarget('nemo');` : On décide de tirer sur l'entité qui a l'id "nemo"
-    // yield* wait();
 
+    // On cherche la cible la plus proche de la base parmi les cibles potentielles et on la sélectionne :
+    selectedTarget = getClosestTarget(possibleTargets)
 
-    // Création de la logique pour définir la cible idéale lors de ce tour :
-    actorsList.forEach(target => {
-      if ( target.type === "robot"
-        && target.alive
-        && target.isInRange)
-        return selectedTarget = target.id
-    })
+    // DEBUG - infos:
+    console.debug(`cibles potentielles du tour n° ${turn}`, possibleTargets)
 
     // Si une cible est sélectionnée, on l'éxecute, Sinon, on passe au prochain tour :
-    selectedTarget ? yield* shotTarget(selectedTarget) : yield* wait()
-
+    if (selectedTarget && turn > 1) {
+      console.debug(`victime au tour n° ${turn} >>>>>`, selectedTarget.id, "<<<<<")
+      yield* shotTarget(selectedTarget.id)
+      base.energy--
+    } else {
+      yield* wait()
+    }
   }
 }
